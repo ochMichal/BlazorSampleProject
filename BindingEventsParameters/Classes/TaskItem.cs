c32d76650d05d0574eef9d0dcfd26b89ac389614
Michal Och <michal.och@pl.abb.com>
2020-07-09 13:50:42 +0200

    Finished project

---
+++ BindingEventsParameters/Classes/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BindingEventsParameters.Classes
+{
+    public class TaskItem
+    {
+        public string TaskDescription { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
